376e81c0005615799768eaca056424d9a7acf221
Krzysztof Kasprzyk <krzychu1232@gmail.com>
2021-07-01 22:15:07 +0200

    Added Recipe Image upload feature

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 from django.conf import settings
-import uuid, os
+import uuid
+import os
 
 
 def recipe_image_file_path(instance, filename):
